65978229c45356ab7aeed197cc1db49bceab306c
Harshit Sharma <166944105+dev-harshitsharma@users.noreply.github.com>
2024-06-04 22:24:29 +0530

    June 04,2024

---
+++ DSA-Concepts/DP/Fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n,vector<int> &dp){
+    if(n <=1 ){
+        return n;
+    }
+    
+    if(dp[n] != -1) return dp[n];
+    
+    return dp[n] = fibo(n-1,dp) + fibo(n-2,dp); //memoization done
+}
+
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Which number you want of the fibonacci series ";
+    cin >> n;
+    
+    vector<int> dp(n+1,-1);
+    cout <<fibo(n,dp);
+
+    return 0;
+}
